cee211617cd3eac8f5b5f246aad527c3c3faaa7d
michamichaa <116735133+michamichaa@users.noreply.github.com>
2023-10-26 19:35:19 -0400

    This is in a branch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Testing
 
 Hello, I am changing the files
+
+Hello, again, I am now changing this again in a branched repository
--- test.cpp
+++ test.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void Trying() {
+    cout << "This is inside a function" << endl;
+}
+
 int main() {
     cout << "Hello World" << endl;
+    Trying;
+    
 
     return 0;
 }
